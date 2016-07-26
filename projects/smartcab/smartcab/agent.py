eff562c8ca7fe4d3e69db76d39fd01ccd71ce0c3
mKarpis <miroslav.karpis@gmail.com>
2016-07-26 13:43:15 +0200

    feat: implementing Task#2 in smartcab project

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -1,4 +1,6 @@
 import random
+import itertools
+import sys
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -6,34 +8,132 @@ from simulator import Simulator
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        
+    def __init__(self, env_):
+        super(LearningAgent, self).__init__(env_)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        self.valid_actions = [None, 'forward', 'left', 'right']
+        self.state = {}
+        self.actions = [None, 'forward', 'left', 'right']
+        self.state_roots = ['light', 'next_waypoint'] #'right', 'left'
+        self.Qtable = {}
+        #tuning variables
+        self.gamma = 0.1 #todo update self.gamma
+        self.alpha = 0.1
+
+
+    def add_new_state_to_Qtable(self, new_state_):
+        print "ADDING NEW STATE: ", new_state_
+        init_actions = [(None, 0), ('forward', 0), ('left', 0), ('right', 0)]
+        state_tuple = {new_state_: init_actions}
+        self.Qtable.update(state_tuple)
+        return init_actions
+
+
+    def print_Qtable(self):
+        print "-----------------------"
+        print "self.Qtable:"
+        for row in self.Qtable:
+            print "\t", row, self.Qtable[row]
+        print "-----------------------"        
+
+
+    def get_action_from_Qtable(self, state_):
+        state_tuple = self.Qtable.get(str(state_))
+        new_action_tuple = None
+        
+        if state_tuple is None:
+            # if the state is not in Q-table create it and choose random action
+            state_actions = self.add_new_state_to_Qtable(state_)
+            new_action_tuple = random.choice(state_actions)
+            print "new_random_action: ", new_action_tuple
+        else:
+            #state is in Q_table so get the maximum
+            print "found_existing_Q-table_state: ", state_tuple
+            i = random.choice([0,1,2,3]);
+            max_value = -sys.maxint - 1
+            for state in state_tuple:
+                if state[1] > max_value:
+                    max_state = i
+                    max_value = state[1]
+            
+            new_action_tuple = state_tuple[i]
+            print "max_state: ", state_tuple[i]
+        return new_action_tuple    
+
+
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
+        print "\n-----resetting-----"
         # TODO: Prepare for a new trip; reset any variables here, if required
+
+
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
+        inputs.update({'next_waypoint': self.next_waypoint})
 
-        # TODO: Update state
+        
+        # Update state
+        print "\n\ninputs:", inputs
+        # get only the monitoring states and save them to self.state     
+        map(lambda state: self.state.update({state: inputs[str(state)]}), self.state_roots)
+        
+        
+        self.print_Qtable()
+                
+        
+        # 1) ----- get new state from Q-table (according to your policy)
+        state_string = str(self.state)
+        print "state_string:", state_string
+        new_action_tuple = self.get_action_from_Qtable(state_string)
+         
 
-        # TODO: Select action according to your policy
-        action = random.choice(self.valid_actions)
-
-        # Execute action and get reward
+        # 2) ----- Execute action and get reward
+        action = new_action_tuple[0]
+        #action = random.choice(self.actions)
+        #action = self.next_waypoint
+        print "new_action_tuple:", new_action_tuple               
         reward = self.env.act(self, action)
+        print "action REWARD: ", reward
+        
+                        
+        # 3) ----- Learn policy based on state, action, reward
+        new_state = {}
+        new_inputs = self.env.sense(self)
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator        
+        new_inputs.update({'next_waypoint': self.next_waypoint})
 
-        # TODO: Learn policy based on state, action, reward
+        
+        map(lambda state: new_state.update({state: new_inputs[str(state)]}), self.state_roots)
+        print "new_state:", new_state
+        max_action_tuple = self.get_action_from_Qtable(str(new_state))
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        
+        Q_hat = (1-self.alpha) * new_action_tuple[1] + self.alpha * (reward + self.gamma * max_action_tuple[1])
+        
+        print "Q_hat:", Q_hat
+        state_tuples = self.Qtable[state_string]
+        
+        updated_state_tuples = state_tuples
+        i = 0
+        for s in state_tuples:
+            if s[0] == action: state_tuples[i] = (action, Q_hat)
+            i += 1
+                
+        print "updated_state_tuples:", updated_state_tuples
+        print "state_string:", state_string
+        self.Qtable[state_string] = updated_state_tuples
+        
+        #print "self.next_waypoint:", self.next_waypoint
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+
 
 
 def run():
@@ -46,7 +146,7 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=1, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
--- projects/smartcab/smartcab_report.md
+++ projects/smartcab/smartcab_report.md
@@ -5,12 +5,81 @@ author: miroslav.karpis@gmail.com
 project: Udacity Reinforcement Learning project (Machine Learning Engineer Nanodegree)
 ```
 
-## Task 1: Randomly select next action
-QUESTION: Observe what you see with the agent's behavior as it takes random actions.
+## Simulation details
+### Valid actions###
+[None, 'forward', 'left', 'right']
 
-1. *Does the smartcab eventually make it to the destination?*
- * During my observations (approximately 10 cycles of new target placement), I have observed only 1 case, when the car reached the target.
-2. *Are there any other interesting observations to note?*
- * Not very much, the behavior is completely random, so I don't think that there is any particular pattern to be found at this stage of the project.
+### Rules
+Agent is at a green light, it should be allowed to:
+* Go forward
+* Turn right
+* Turn left, yielding to any oncoming traffic that is going straight or turning right
+
+At a red light:
+* Turn right, yielding to any traffic from the left that is going straight
+
+### Rewards and Goal
+* The smartcab receives a reward for each successfully completed trip.
+* Smaller reward for each action it executes successfully that obeys traffic rules. 
+* Smartcab receives a small penalty for any incorrect action.
+* Larger penalty for any action that violates traffic rules or causes an accident with another vehicle.
+
+
+## Task 1: Implement a Basic Driving Agent
+
+**Question 1: Does the smartcab eventually make it to the destination?**
+During my observations (approximately 10 cycles of new target placement), I have observed only 1 case, when the car reached the target.
+
+**Question 2: Are there any other interesting observations to note?**
+So far I didn't observe very much of interesting behavior.
+
+
+## Task 2: Inform the Driving Agent
+
+**Question 1: What states have you identified that are appropriate for modeling the smartcab and environment?**
+
+ Initially we have following state inputs available from the environment:
+ * **light**: ['red', 'green']
+ 
+ * **oncoming**: Information whether a car is approaching from oncoming(forward) direction, and the direction where the vehicle is heading. [None, 'right', 'left', 'forward']
+ 
+ * **right**: Information whether a car is approaching from right direction, and the direction where the vehicle is heading. [None, 'right', 'left', 'forward']
+ 
+ * **left**: Information whether a car is approaching from left direction, and the direction where the vehicle is heading. [None, 'right', 'left', 'forward']
+ 
+ * **deadline**: The current time left from the allotted deadline.
+ 
+ * **t**: Total of my current time steps (in this game)
+ 
+ * **reward**: Sum of my current reward
+ 
+ * **waypoint**: The next waypoint location relative to its current location and heading.  ['right', 'left', 'forward']
+ 
+ I think that following states are important and should be identified as appropriate for modeling the smartcab (reasons described in question 2):
+  * light, oncoming, right, left
+
+**Question 2: Why do you believe each of these states to be appropriate for this problem?**
+ Most of the selected states because of the specified rules in overall [rules](#rules). 
+ 
+ List of selected states and the reason of selecting it:
+ 
+ * **light**: State indicating whether we can go to all directions (on green light, with extra check when we need to turn to left), or only to right (on red light).
+ 
+ * **oncoming**: Important state in relation to traffic light. Logic described in [rules](#rules).
+ 
+ * **right**: Important state in relation to traffic light. Logic described in [rules](#rules).
+ 
+ * **left**: Important state in relation to traffic light. Logic described in [rules](#rules).
+ 
+ 
+**OPTIONAL**
+1. *How many states in total exist for the smartcab in this environment?* If we consider that our states have following parameters/combinations we have total **129 states**. 129 is calculated from following values (2 x lights) x (4 x oncoming) x (4 x right) x (4 x left)
+2. Does this number seem reasonable given that the goal of Q-Learning is to learn and make informed decisions about each state? 
+3. Why or why not?
+
+
+## Task 3: Implement a Q-Learning Driving Agent
+
+### QUESTION: What changes do you notice in the agent's behavior when compared to the basic driving agent when random actions were always taken? Why is this behavior occurring?
 
 
