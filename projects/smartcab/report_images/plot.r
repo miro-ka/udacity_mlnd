beb44094881e724ae3a6a3c483a1a733aaec8534
mKarpis <miroslav.karpis@gmail.com>
2016-07-31 01:11:56 +0200

    feat: adding smartcab plot

---
+++ projects/smartcab/report_images/plot.r
@@ -0,0 +1,42 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fnil\fcharset0 AndaleMono;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red249\green249\blue249;\red52\green52\blue52;\red0\green0\blue0;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww16280\viewh4200\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural
+
+\f0\fs20 \cf0 \cb1 \CocoaLigature0 \
+par(mfrow = c(2,3))\
+plot(df$gamma, df$success, col="red\cb1 "\cb1 ,\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1  xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 gamma\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 success\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+plot(df$alpha, df$success, col=\cb1 "\cb1 green\cb1 ", \cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 alpha\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 success\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+plot(df$epsilon, df$success, col=\cb1 "\cb1 blue\cb1 ", \cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 epsilon\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 success\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+plot(df$gamma, df$total_ierations, col="red\cb1 "\cb1 , \cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 gamma\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 total iterations\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+plot(df$alpha, df$total_ierations, col=\cb1 "\cb1 green\cb1 ", \cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 alpha\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 total iterations\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+plot(df$epsilon, df$total_ierations, col=\cb1 "\cb1 blue\cb1 ", \cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 xlab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 epsilon\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 ", ylab="\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 total iterations\cb2 \expnd0\expndtw0\kerning0
+\CocoaLigature1 "\cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 )\
+\pard\pardeftab720
+
+\f1\fs24 \cf4 \cb1 \expnd0\expndtw0\kerning0
+\CocoaLigature1 \outl0\strokewidth0 \strokec4 title("My Title")\
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural
+
+\f0\fs20 \cf0 \cb1 \kerning1\expnd0\expndtw0 \CocoaLigature0 \outl0\strokewidth0 \
+\
+}
